c04f4ec5c6eaa78511c65a2929f74badaa7b8939
Ubuntu <ubuntu@ip-172-31-90-67.ec2.internal>
2019-10-27 21:17:31 +0000

    first commit

--- factorialloop.cpp
+++ factorialloop.cpp
@@ -12,18 +12,24 @@ main() {
     // This function adds all numbers using a loop
 
     // variables
+    std::string value_1_string;
     int value_1;
     int answer = 1;
     int adder = 0;
 
     // input
     std::cout << "Input a number of your choice:" << std::endl;
-    std::cin >> value_1;
+    std::cin >> value_1_string;
 
     // process and output
-    do {
-        adder += 1;
-        answer = answer * adder;
-    } while (adder < value_1);
-    std::cout << "The answer is " << answer << "." << std::endl;
+    try {
+        value_1 = std::stoi(value_1_string);
+        do {
+            adder = adder + 1;
+            answer = answer * adder;
+        } while (adder < value_1);
+        std::cout << "The product is " << answer << std::endl;
+    } catch (std::invalid_argument) {
+            std::cout << "Please input a valid positive integer" << std::endl;
+    }
 }
